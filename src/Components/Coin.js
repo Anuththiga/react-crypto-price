1d86d32e1513f377c3f8e4ddfe5b453856bf939d
Tanuththiga <anuththiga25jt@gmail.com>
2023-09-13 14:05:32 +0800

    coin styles

--- src/Components/Coin.js
+++ src/Components/Coin.js
@@ -7,15 +7,16 @@ const Coin = ({ id, coinName, icon, symbol, price, priceChange, marketCap }) => 
         <div className="coin-data">
           <div className="coin">
             <img src={icon} />
-            <h1>{coinName}</h1>
-            <p>{symbol}</p>
-            <p>{price.toFixed(2)}</p>
+            <h1 className="coin-name">{coinName}</h1>
+            <p className="coin-symbol">{symbol}</p>
+            <p className="coin-price">{price.toFixed(2)}</p>
             {priceChange < 0 ? (
               <p className="price-change red">{priceChange.toFixed(2)}%</p>
             ) : (
               <p className="price-change green">{priceChange.toFixed(2)}%</p>
             )}
-            <p>$ {marketCap.toLocaleString()}</p>
+            <p className="coin-volume">$ {marketCap.toLocaleString()}</p>
+            <button>More Info</button>
           </div>
         </div>
       </div>
